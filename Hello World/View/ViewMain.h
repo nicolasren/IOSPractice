fcd3d1db46b594d1f3b8aafa7d1af5382984a764
nicolas <xianglinren@gmail.com>
2017-09-20 18:07:57 +0800

    第二次提交

---
+++ Hello World/View/ViewMain.h
@@ -0,0 +1,13 @@
+//
+//  ViewMain.h
+//  Hello World
+//
+//  Created by nicolas on 2017/9/20.
+//  Copyright © 2017年 nicolas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewMain : UIView
+
+@end
---
+++ Hello World/View/ViewMain.m
@@ -0,0 +1,21 @@
+//
+//  ViewMain.m
+//  Hello World
+//
+//  Created by nicolas on 2017/9/20.
+//  Copyright © 2017年 nicolas. All rights reserved.
+//
+
+#import "ViewMain.h"
+
+@implementation ViewMain
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
