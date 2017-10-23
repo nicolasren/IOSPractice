87936e34dc50b5797c06bbe0c3ab400c7c01273c
nicolas <xianglinren@gmail.com>
2017-10-23 10:32:06 +0800

    重新提交书籍的数据结构

---
+++ Hello World/Reader/BookModel.h
@@ -0,0 +1,18 @@
+//
+//  BookModel.h
+//
+//  书籍的数据结构
+//
+//  Created by nicolas on 2017/10/21.
+//  Copyright © 2017年 nicolas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BookModel : NSObject
+
+@property (nonatomic, strong)NSString *bookName; //书名
+@property (nonatomic, strong)NSString *bookCoverUrl; //书籍封面URL
+
+@end
+
---
+++ Hello World/Reader/BookModel.m
@@ -0,0 +1,14 @@
+//
+//  BookModel.m
+//  Hello World
+//
+//  Created by nicolas on 2017/10/21.
+//  Copyright © 2017年 nicolas. All rights reserved.
+//
+
+#import "BookModel.h"
+
+@implementation BookModel
+
+@end
+
